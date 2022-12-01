217f53996ab62266ae1a429679976a6ce9a31faf
Juan C Parra <jcamiloparra7.0@gmail.com>
2022-11-30 22:39:58 -0500

    feat: Pull catalog options from snowflake

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,11 +1,17 @@
 import streamlit
 import snowflake.connector
+import pandas
 
 snow_conn = snowflake.connector.connect(**streamlit.secrets['snowflake'])
-
-streamlit.title("Hello, im a streamlit app")
-
 snow_cur = snow_conn.cursor()
+
+streamlit.title("Zena\'s Amazing Athleisure Catalog")
+snow_cur.execute("SELECT COLOR_OR_STYLE FROM CATALOG_FOR_WEBSITE")
+catalog_options = snow_cur.fetchall()
+streamlit.text(catalog_options)
+
+
+
 
 snow_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
