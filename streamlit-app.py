c423bc79bb1d973c41762271d3e3634438c4d4a8
Juan C Parra <jcamiloparra7.0@gmail.com>
2022-11-30 22:27:52 -0500

    feat: Add snowflake connection

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,15 @@
 import streamlit
+import snowflake.connector
+
+snow_conn = snowflake.connector.connect(**streamlit.secrets['snowflake'])
 
 streamlit.title("Hello, im a streamlit app")
+
+snow_cur = snow_conn.cursor()
+
+snow_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+results = snow_cur.fetchall()
+
+streamlit.text("Calling snowflake now:")
+streamlit.text(results)
