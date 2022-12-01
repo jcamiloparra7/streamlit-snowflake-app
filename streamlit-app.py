01e4393fe8c646ed57bf1602d6e45dfb1fa51b30
Juan C Parra <jcamiloparra7.0@gmail.com>
2022-11-30 23:17:42 -0500

    feat: Add image posting functionality

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,18 +6,24 @@ snow_conn = snowflake.connector.connect(**streamlit.secrets['snowflake'])
 snow_cur = snow_conn.cursor()
 
 streamlit.title("Zena\'s Amazing Athleisure Catalog")
-snow_cur.execute("SELECT COLOR_OR_STYLE FROM CATALOG_FOR_WEBSITE")
+snow_cur.execute("SELECT COLOR_OR_STYLE FROM CATALOG_FOR_WEBSITE;")
 catalog_options = snow_cur.fetchall()
-catalog_options = pandas.DataFrame(catalog_options)
+# catalog_options = pandas.DataFrame(catalog_options)
 streamlit.write(catalog_options)
 catalog_option_list = catalog_options.iloc[:, 0].values.tolist()
-streamlit.selectbox('Pick a sweatsuit color or style:', catalog_option_list)
+chosen_option = streamlit.selectbox('Pick a sweatsuit color or style:', catalog_option_list)
 # streamlit.text(catalog_options)
-
+snow_cur.execute(f"SELECT DIRECT_URL, PRICE, SIZE_LIST, UPSELL_PRODUCT_DESC FROM CATALOG_FOR_WEBSITE WHERE COLOR_OR_STYLE = {chosen_option};")
+option_attr = snow_cur.fetchone()
+image_caption = f'Our warm, comfortable, {chosen_option} sweatsuit!'
+streamlit.image(option_attr[0], width=400, caption=image_caption)
+streamlit.text(f'Price: {option_attr[1]}')
+streamlit.text(f'Sizes Available: {option_attr[2]}')
+streamlit.text(option_attr[3])
 
 snow_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
 results = snow_cur.fetchall()
 
-streamlit.text("Calling snowflake now:")
-streamlit.text(results)
+# streamlit.text("Calling snowflake now:")
+# streamlit.text(results)
